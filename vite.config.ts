83cfef22952c4790d27a8ded4943987131085610
DutchPayEasy_simple <code.daniel85@gmail.com>
2025-07-26 12:26:58 +0900

    #250726. 외부 모듈로 전환환

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,13 @@ export default defineConfig({
     },
   },
   plugins: [reactRouter(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      external: [
+        // 'react-email-starter/emails/welcome-user'를 외부 모듈로 지정
+        'react-email-starter/emails/welcome-user',
+        // 다른 외부화할 모듈이 있다면 여기에 추가
+      ],
+    },
+  },
 });
