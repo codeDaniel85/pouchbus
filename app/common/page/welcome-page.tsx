b31f3479eb6dc005e1831784f2d3a5892376a654
DutchPayEasy_simple <code.daniel85@gmail.com>
2025-07-26 18:03:12 +0900

    #250726. react-email-starter 임시 사용 중지(./node_modules 용량 너무 커서 github ignore 처리함.)

--- app/common/page/welcome-page.tsx
+++ app/common/page/welcome-page.tsx
@@ -10,8 +10,8 @@ export const loader = async ({ params }: Route.LoaderArgs) => {
         from: "Pouch Bus <mail.pouchbus@mail.pouchbus.com>",
         to: ["code.daniel85@gmail.com"],
         subject: "[파우치버스] 고된 출퇴근길, 작은 변화의 시작",
-        //text: "hello world",
-        react: <WelcomeEmail username={"Daniel85"} />,
+        text: "hello world",
+        //react: <WelcomeEmail username={"Daniel85"} />,
     });
     return Response.json({ data, error: null });
 }
